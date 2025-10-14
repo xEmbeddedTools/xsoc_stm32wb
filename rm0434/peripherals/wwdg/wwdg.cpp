25ffdbc1a64706838f8349b9cb66f5d4de7c16bd
krzysiek-b-y <krzysztof.borowy@heavykinematic.com>
2025-10-14 09:48:18 +0200

    add wwdg (#39)
    window watchdog

---
+++ rm0434/peripherals/wwdg/wwdg.cpp
@@ -0,0 +1,49 @@
+/*
+ *  Copyright (c) xEmbeddedTools team and contributors.
+ *  Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for details.
+ */
+
+// this
+#include <rm0434/peripherals/wwdg/wwdg.hpp>
+
+using namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals;
+
+void wwdg::enable(wwdg::window_t a_window)
+{
+    WWDG->CFR = WWDG_CFR_WDGTB | a_window;
+    WWDG->CR = WWDG_CR_WDGA | WWDG_CR_T;
+}
+
+void wwdg::feed()
+{
+    static constexpr uint32_t reload = WWDG_CR_T;
+    static_assert(reload <= window_max && reload >= window_min, "invalid reload value");
+    WWDG->CR = WWDG_CR_WDGA | reload;
+}
+
+void wwdg::feed(wwdg::window_t a_reload)
+{
+    WWDG->CR = a_reload | WWDG_CR_WDGA;
+}
+
+bool wwdg::is_active()
+{
+    return xmcu::bit::is_any(WWDG->CR, WWDG_CR_WDGA);
+}
+
+void wwdg::interrupt_enable(const xmcu::hal::IRQ_config& a_config)
+{
+    bit::flag::set(&WWDG->CFR, WWDG_CFR_EWI);
+
+    NVIC_SetPriority(WWDG_IRQn,
+                     NVIC_EncodePriority(NVIC_GetPriorityGrouping(), a_config.preempt_priority, a_config.sub_priority));
+    NVIC_EnableIRQ(WWDG_IRQn);
+}
+
+void wwdg::feed_int()
+{
+    // This write operation must be performed a few instructions before the end of the ISR.
+    // If it is the final instruction before exiting, it could cause an immediate re-entry into this same IRQ.
+    WWDG->SR = 0; // clear interrupt flag
+    wwdg::feed();
+}
---
+++ rm0434/peripherals/wwdg/wwdg.hpp
@@ -0,0 +1,65 @@
+#pragma once
+
+/*
+ *  Copyright (c) xEmbeddedTools team and contributors.
+ *  Licensed under the Apache License, Version 2.0. See LICENSE file in the project root for details.
+ */
+
+// std
+#include <stdint.h>
+
+// CMSIS
+#include <stm32wbxx.h>
+
+// xmcu
+#include <rm0434/rcc.hpp>
+#include <xmcu/hal/IRQ_config.hpp>
+#include <xmcu/Limited.hpp>
+#include <xmcu/non_constructible.hpp>
+
+namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals {
+
+class wwdg : private xmcu::non_constructible
+{
+public:
+    static constexpr uint32_t window_min = 0x40;
+    static constexpr uint32_t window_max = 0x7F;
+    using window_t = xmcu::Limited<uint32_t, window_min, window_max>;
+
+    static void enable(window_t a_window = window_max);
+
+    static void feed();
+    static void feed(window_t reload);
+    static bool is_active();
+
+protected:
+    static void interrupt_enable(const xmcu::hal::IRQ_config& a_config);
+    static void feed_int();
+};
+
+} // namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals
+
+namespace xmcu::soc::st::arm::m4::wb::rm0434 {
+template<std::uint32_t id> class rcc<peripherals::wwdg, id> : private non_constructible
+{
+public:
+    static void enable(bool a_enable_in_lp)
+    {
+        bit::flag::set(&RCC->APB1ENR1, RCC_APB1ENR1_WWDGEN);
+
+        if (true == a_enable_in_lp)
+        {
+            bit::flag::set(&RCC->APB1SMENR1, RCC_APB1SMENR1_WWDGSMEN);
+        }
+        else
+        {
+            bit::flag::clear(&RCC->APB1SMENR1, RCC_APB1SMENR1_WWDGSMEN);
+        }
+    }
+    static void disable()
+    {
+        bit::flag::clear(&RCC->APB1ENR1, RCC_APB1ENR1_WWDGEN);
+    }
+};
+
+} // namespace xmcu::soc::st::arm::m4::wb::rm0434
