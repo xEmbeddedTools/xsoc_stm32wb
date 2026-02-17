96abc78b415948a9406a3832a7394bd06bb16d98
Mariusz Matkowski <mariusz.matkowski@heavykinematic.com>
2026-02-17 13:54:49 +0100

    wip

--- rm0434/peripherals/AES/AES.cpp
+++ rm0434/peripherals/AES/AES.cpp
@@ -7,22 +7,61 @@
 #include "stm32wb55xx.h"
 #include <rm0434/peripherals/AES/AES.hpp>
 
+// std
+#include <cstring>
+#include <utility>
+
 // xmcu
+#include <xmcu/Duration.hpp>
 #include <xmcu/bit.hpp>
-
-
 
 namespace {
 
 using namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals;
 
-void enable(AES_TypeDef* a_p_registers)
+void load_key(AES_TypeDef* p_registers, const std::uint32_t* p_key, AES::Key_size key_size)
 {
-    bit::flag::clear(&(a_p_registers->CR), AES_CR_EN_Pos);
-        // a_p_registers->CR
+    if (!p_key) return;
+
+    p_registers->KEYR0 = p_key[0];
+    p_registers->KEYR1 = p_key[1];
+    p_registers->KEYR2 = p_key[2];
+    p_registers->KEYR3 = p_key[3];
+
+    if (key_size == AES::bits_256)
+    {
+        p_registers->KEYR4 = p_key[4];
+        p_registers->KEYR5 = p_key[5];
+        p_registers->KEYR6 = p_key[6];
+        p_registers->KEYR7 = p_key[7];
+    }
 }
 
-void disable(AES_TypeDef* a_p_registers)
+void write_block(AES_TypeDef* p_registers, const std::uint8_t* p_data)
+{
+    std::uint32_t buffer[4];
+
+    std::memcpy(buffer, p_data, 16);
+
+    p_registers->DINR = buffer[0];
+    p_registers->DINR = buffer[1];
+    p_registers->DINR = buffer[2];
+    p_registers->DINR = buffer[3];
+}
+
+void read_block(AES_TypeDef* p_registers, std::uint8_t* p_out)
+{
+    std::uint32_t buffer[4];
+
+    buffer[0] = p_registers->DOUTR;
+    buffer[1] = p_registers->DOUTR;
+    buffer[2] = p_registers->DOUTR;
+    buffer[3] = p_registers->DOUTR;
+
+    std::memcpy(p_out, buffer, 16);
+}
+
+AES::Pooling::Status wait_for_ccf(AES_TypeDef* p_registers, xmcu::Milliseconds a_timeout)
 {
 
 }
@@ -31,19 +70,60 @@ void disable(AES_TypeDef* a_p_registers)
 
 namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals {
 
-using namespace utils;
 using namespace xmcu;
+
+template<> AES::Pooling::Result<AES::ecb_encrypt> AES::Pooling::execute(const Context<AES::ecb_encrypt>& a_ctx,
+                                                                        xmcu::Milliseconds               a_timeout)
+{
+    AES_TypeDef* regs = this->p_aes->p_registers;
+
+    this->p_aes->disable();
+
+    std::uint32_t cr_val = 0u;
+
+    bit::flag::set(&cr_val, std::to_underlying(AES::ecb_encrypt));
+    bit::flag::set(&cr_val, std::to_underlying(a_ctx.key_size));
+    bit::flag::set(&cr_val, std::to_underlying(a_ctx.data_type));
+
+    regs->CR = cr_val;
+
+    load_key(regs, a_ctx.p_key, a_ctx.key_size);
+
+    this->p_aes->enable();
+
+    const std::size_t num_blocks = a_ctx.size / 16;
+    const std::uint8_t* in_ptr = a_ctx.p_input;
+    std::uint8_t* out_ptr = a_ctx.p_output;
+
+    for (std::size_t i = 0; i < num_blocks; ++i)
+    {
+        write_block(regs, in_ptr);
+
+        if (wait_for_ccf(regs, a_timeout) != Status::ok)
+        {
+            this->p_aes->disable();
+            return { .status = Status::timeout };
+        }
+
+        read_block(regs, out_ptr);
+
+        bit::flag::set(&(regs->CR), AES_CR_CCFC);
+    }
+
+    this->p_aes->disable();
+
+    return { .status = Status::ok };
+}
 
 void AES::enable()
 {
-    ::enable(this->p_registers);
+    bit::flag::set(&(this->p_registers->CR), AES_CR_EN);
 }
 
 void AES::disable()
 {
-    ::disable(this->p_registers);
+    bit::flag::clear(&(this->p_registers->CR), AES_CR_EN);
 }
-
 
 } // namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals
 
@@ -62,5 +142,3 @@ void rcc<AES, AES::_2>::disable()
 }
 
 } // namespace xmcu::soc::st::arm::m4::wb::rm0434
-
-
--- rm0434/peripherals/AES/AES.hpp
+++ rm0434/peripherals/AES/AES.hpp
@@ -9,15 +9,16 @@
 #include <stm32wbxx.h>
 
 // xmcu
-#include <rm0434/rcc.hpp>
 #include <rm0434/config.hpp>
-
+#include <rm0434/rcc.hpp>
+#include <xmcu/Duration.hpp>
 #include <xmcu/Non_copyable.hpp>
-
+#include <xmcu/bit.hpp>
 
 namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals {
 
-class AES : private xmcu::Non_copyable {
+class AES : private xmcu::Non_copyable
+{
 public:
 #if defined(XMCU_AES1_PRESENT)
     enum class _1;
@@ -28,51 +29,22 @@ public:
 
     enum class Mode : std::uint32_t
     {
-        // ====================================================================
-        // 1. Electronic Codebook (ECB)
-        // ====================================================================
-        // Simplest mode. Block-by-block, no chaining.
-        ecb_encrypt           = 0u,                                                      // CHMOD:000, MODE:00
-        ecb_key_derivation    = AES_CR_MODE_0,                                           // CHMOD:000, MODE:01
-        ecb_decrypt           = AES_CR_MODE_1,                                           // CHMOD:000, MODE:10
-        ecb_key_deriv_decrypt = AES_CR_MODE_0 | AES_CR_MODE_1,                           // CHMOD:000, MODE:11
-
-        // ====================================================================
-        // 2. Cipher Block Chaining (CBC)
-        // ====================================================================
-        // Uses Initialization Vector (IV).
-        cbc_encrypt           = AES_CR_CHMOD_0,                                          // CHMOD:001, MODE:00
-        // Note: Hardware ignores CHMOD in Mode 2, but we define it for API consistency
-        cbc_key_derivation    = AES_CR_CHMOD_0 | AES_CR_MODE_0,                          // CHMOD:001, MODE:01
-        cbc_decrypt           = AES_CR_CHMOD_0 | AES_CR_MODE_1,                          // CHMOD:001, MODE:10
-        cbc_key_deriv_decrypt = AES_CR_CHMOD_0 | AES_CR_MODE_0 | AES_CR_MODE_1,          // CHMOD:001, MODE:11
-
-        // ====================================================================
-        // 3. Counter Mode (CTR)
-        // ====================================================================
-        // Stream cipher. Key Derivation (Mode 2) is forbidden.
-        // Decryption is physically the same as encryption (generating keystream),
-        // but we expose both for API logic.
-        ctr_encrypt           = AES_CR_CHMOD_1,                                          // CHMOD:010, MODE:00
-        ctr_decrypt           = AES_CR_CHMOD_1 | AES_CR_MODE_1,                          // CHMOD:010, MODE:10
-
-        // ====================================================================
-        // 4. Galois/Counter Mode (GCM) & GMAC
-        // ====================================================================
-        // Authenticated Encryption. Mode 2 and 4 are forbidden.
-        // GMAC uses the same hardware config as GCM (011) but handles data differently.
-        gcm_encrypt           = AES_CR_CHMOD_0 | AES_CR_CHMOD_1,                         // CHMOD:011, MODE:00
-        gcm_decrypt           = AES_CR_CHMOD_0 | AES_CR_CHMOD_1 | AES_CR_MODE_1,         // CHMOD:011, MODE:10
-        
-        gmac_encrypt          = gcm_encrypt,                                             // Alias
-        gmac_decrypt          = gcm_decrypt,                                             // Alias
-
-        // ====================================================================
-        // 5. Counter with CBC-MAC (CCM)
-        // ====================================================================
-        // Uses Bit 16 for CHMOD[2]. Mode 2 and 4 are forbidden.
-        ccm_encrypt           = AES_CR_CHMOD_2,                                          // CHMOD:100, MODE:00
-        ccm_decrypt           = AES_CR_CHMOD_2 | AES_CR_MODE_1,                          // CHMOD:100, MODE:10
+        ecb_encrypt           = 0u,                                              // CHMOD:000, MODE:00
+        ecb_key_derivation    = AES_CR_MODE_0,                                   // CHMOD:000, MODE:01
+        ecb_decrypt           = AES_CR_MODE_1,                                   // CHMOD:000, MODE:10
+        ecb_key_deriv_decrypt = AES_CR_MODE_0 | AES_CR_MODE_1,                   // CHMOD:000, MODE:11
+        cbc_encrypt           = AES_CR_CHMOD_0,                                  // CHMOD:001, MODE:00
+        cbc_key_derivation    = AES_CR_CHMOD_0 | AES_CR_MODE_0,                  // CHMOD:001, MODE:01
+        cbc_decrypt           = AES_CR_CHMOD_0 | AES_CR_MODE_1,                  // CHMOD:001, MODE:10
+        cbc_key_deriv_decrypt = AES_CR_CHMOD_0 | AES_CR_MODE_0 | AES_CR_MODE_1,  // CHMOD:001, MODE:11
+        ctr_encrypt           = AES_CR_CHMOD_1,                                  // CHMOD:010, MODE:00
+        ctr_decrypt           = AES_CR_CHMOD_1 | AES_CR_MODE_1,                  // CHMOD:010, MODE:10
+        gcm_encrypt           = AES_CR_CHMOD_0 | AES_CR_CHMOD_1,                 // CHMOD:011, MODE:00
+        gcm_decrypt           = AES_CR_CHMOD_0 | AES_CR_CHMOD_1 | AES_CR_MODE_1, // CHMOD:011, MODE:10
+        gmac_encrypt          = gcm_encrypt,                                     // Alias
+        gmac_decrypt          = gcm_decrypt,                                     // Alias
+        ccm_encrypt           = AES_CR_CHMOD_2,                                  // CHMOD:100, MODE:00
+        ccm_decrypt           = AES_CR_CHMOD_2 | AES_CR_MODE_1,                  // CHMOD:100, MODE:10
     };
 
     enum class Key_size : std::uint32_t
@@ -81,23 +53,81 @@ public:
         bits_256 = AES_CR_KEYSIZE,
     };
 
+    enum class Data_type : std::uint32_t
+    {
+        full_word = 0u,
+        half_word = AES_CR_DATATYPE_0,
+        byte      = AES_CR_DATATYPE_1,
+        bit       = AES_CR_DATATYPE_0 | AES_CR_DATATYPE_1,
+    };
+
+    using enum Mode;
+    using enum Key_size;
+    using enum Data_type;
+
+    template<Mode> struct Context;
+
+    class Pooling : private Non_copyable
+    {
+    public:
+        enum class Status
+        {
+            ok,
+            timeout,
+        };
+
+        template<Mode> struct Result;
+
+        using enum Status;
+
+        template<Mode mode> Result<mode> execute(const Context<mode>& a_ctx, xmcu::Milliseconds a_timeout);
+
+    private:
+        AES* p_aes;
+        friend AES;
+    };
+
+    Pooling pooling;
+
+private:
+    AES(AES_TypeDef* a_p_registers, IRQn_Type a_irqn)
+        : p_registers(a_p_registers)
+        , irqn(a_irqn)
+    {
+        this->pooling.p_aes = this;
+    }
+
     void enable();
     void disable();
 
-private:
-    AES(std::uint32_t a_idx, AES_TypeDef* a_p_registers, IRQn_Type a_irqn)
-        : idx(a_idx)
-        , p_registers(a_p_registers)
-        , irqn(a_irqn)
-    {
-
-    }
-
-    std::uint32_t idx;
     AES_TypeDef* p_registers;
-    IRQn_Type irqn;
+    IRQn_Type    irqn;
 
     template<typename Periph_t, std::uint32_t id> friend class xmcu::soc::peripheral;
+};
+
+template<> struct AES::Context<AES::Mode::ecb_encrypt>
+{
+    AES::Key_size  key_size;
+    AES::Data_type data_type = AES::Data_type::byte;
+
+    const std::uint32_t* p_key;    // Pointer to Key (4 or 8 words)
+    const std::uint8_t*  p_input;  // Plaintext (Must be multiple of 16 bytes)
+    std::uint8_t*        p_output; // Ciphertext buffer
+    std::size_t          size;     // Size in bytes
+};
+
+template<> struct AES::Context<AES::Mode::ecb_decrypt> : public AES::Context<AES::Mode::ecb_encrypt>
+{
+};
+
+template<> struct AES::Pooling::Result<AES::Mode::ecb_encrypt>
+{
+    AES::Pooling::Status status = AES::Pooling::Status::timeout;
+};
+
+template<> struct AES::Pooling::Result<AES::Mode::ecb_decrypt> : AES::Pooling::Result<AES::Mode::ecb_encrypt>
+{
 };
 
 } // namespace xmcu::soc::st::arm::m4::wb::rm0434::peripherals
@@ -133,7 +163,7 @@ template<> class peripheral<st::arm::m4::wb::rm0434::peripherals::AES, st::arm::
 public:
     static st::arm::m4::wb::rm0434::peripherals::AES create()
     {
-        return st::arm::m4::wb::rm0434::peripherals::AES(0u, AES1, IRQn_Type::AES1_IRQn);
+        return st::arm::m4::wb::rm0434::peripherals::AES(AES1, IRQn_Type::AES1_IRQn);
     }
 };
 #endif
@@ -145,7 +175,7 @@ template<> class peripheral<st::arm::m4::wb::rm0434::peripherals::AES, st::arm::
 public:
     static st::arm::m4::wb::rm0434::peripherals::AES create()
     {
-        return st::arm::m4::wb::rm0434::peripherals::AES(1u, AES2, IRQn_Type::AES2_IRQn);
+        return st::arm::m4::wb::rm0434::peripherals::AES(AES2, IRQn_Type::AES2_IRQn);
     }
 };
 #endif
